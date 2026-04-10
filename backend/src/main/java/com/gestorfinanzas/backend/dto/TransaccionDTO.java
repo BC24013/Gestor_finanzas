6c900c5f1fafa5d6b278a2fc916eb357e2bc85a0
Jonas Eduardo Villalobos Moran <vm24042@ues.edu.sv>
2026-04-09 21:15:53 -0600

    feat: crear TransaccionDTO

---
+++ backend/src/main/java/com/gestorfinanzas/backend/dto/TransaccionDTO.java
@@ -0,0 +1,17 @@
+package com.gestorfinanzas.backend.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class TransaccionDTO {
+    private Long id;
+    private String descripcion;
+    private BigDecimal monto;
+    private LocalDate fecha;
+    private String tipo;
+    private Long usuario;
+    private Long categoria;
+}
